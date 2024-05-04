ef0b214ceb135a5c710e0a1cf44a0c2d042fa720
HANanPANG <3326365030@qq.com>
2024-05-04 21:42:12 +0800

    Site updated: 2024-05-04 21:42:11

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/05/02/testdemo/","2024/05/02/hello-world/"];function toRandomPost(){
+var posts=["2024/05/02/hello-world/","2024/05/02/testdemo/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
