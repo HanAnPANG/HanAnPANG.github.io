5a21b5fa25c25ebf22c94dc42a03d98b00109eeb
HANanPANG <3326365030@qq.com>
2024-05-04 22:21:07 +0800

    Site updated: 2024-05-04 22:21:07

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/05/02/hello-world/","2024/05/02/testdemo/"];function toRandomPost(){
+var posts=["2024/05/02/hello-world/","2024/05/02/51单片机基础涵盖/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
